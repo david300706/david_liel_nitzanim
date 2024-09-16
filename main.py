2f14385c8dea95f0c2f0caeab152c4b3140a262a
liel <lielkassa12@gmail.com>
2024-09-16 14:41:02 +0300

    main init

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import pygame
 import screen
 import time
 
-state = {"bushes":game_field.bush_spread(),
+state = {"bushes": game_field.bush_spread(),
          "showed_welcome_massage": False,
          "game_running": True,
          "soldier_location": [0, 0],
+         "soldier_feet_location": []
          }
 
 
@@ -20,7 +21,6 @@ def main():
     pygame.display.update()
     time.sleep(3)
     while state["game_running"]:
-
         user_events()
 
         screen.draw_game(state)
@@ -35,19 +35,26 @@ def user_events():
 
         if event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and state["soldier_location"][1] > 0:
                 print("Key UP has been pressed")
+                state["soldier_location"][1] -= consts.FRAME_HEIGHT
 
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN and state["soldier_location"][
+                1] < consts.SCREEN_HEIGHT - consts.SOLDIER_Y_LENGTH:
                 print("Key DOWN has been pressed")
+                state["soldier_location"][1] += consts.FRAME_HEIGHT
 
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT and state["soldier_location"][
+                0] < consts.SCREEN_WIDTH - consts.SOLDIER_X_LENGTH:
                 print("Key RIGHT has been pressed")
+                state["soldier_location"][0] += consts.FRAME_WIDTH
 
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and state["soldier_location"][0] > 0:
                 print("Key LEFT has been pressed")
+                state["soldier_location"][0] -= consts.FRAME_WIDTH
+
+
 user_events()
 
-
-soldier.soldier_feet_cords(state["soldier_location"])
+# soldier.soldier_feet_cords(state["soldier_location"])
 main()
--- soldier.py
+++ soldier.py
@@ -1,9 +1,13 @@
 import game_field
 
 
-def soldier_feet_cords(state):
+def soldier_feet_cords(soldier, feet):
     soldier_feet = [[0, 0], [-1, 0]]
-    soldier_feet[1] = state[1] + 2
-    soldier_feet[0] = state[0] + 1
+    soldier_feet[1] = soldier[1] + 2
+    soldier_feet[0] = soldier[0] + 1
     # return soldier_feet
     print(soldier_feet)
+
+# def is_eliminated(state):
+#     index1 = soldier_feet_cords(stat
+#     if soldier_feet_cords(state) == "mine" in game_field:
